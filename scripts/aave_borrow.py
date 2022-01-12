9e3d7c9c6d268e2c7b17d947efd51929c656ad9e
Shubh Pathak <shubhpathak07@gmail.com>
2022-01-13 02:15:44 +0530

    feat:Implemented Collateral Deposit

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,10 +1,40 @@
 from scripts.helpers import get_account
 from scripts.get_weth import get_weth
-from brownie import config,network
+from brownie import config, network, interface
+from web3 import Web3
+
+# 0.1
+amount = Web3.toWei(0.1, "ether")
+
 
 def main():
     account = get_account()
-    weth_address = config["networks"][network.show_active()]["weth_token"]
-    if(network.show_active() in ["mainnet-fork"]):
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
         get_weth()
-    
+    lending_pool = get_lending_pool()
+    print(lending_pool)
+    # Approve sending out ERC20 Tokens
+    appove_erc20_token(amount, lending_pool.address, erc20_address, account)
+    print("Depositing...")
+    tx = lending_pool.deposit(erc20_address,amount,account.address,0,{'from':account})
+    tx.wait(1)
+    print("Deposited!")
+
+
+def appove_erc20_token(amount, spender, erc20_address, account):
+    print("Approving ERC20 Token")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
+
+
+def get_lending_pool():
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
